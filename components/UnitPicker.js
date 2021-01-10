c204114a19010ef50ee717ecdddfa9698b44b03e
Evyatar Haim <evyataros1@gmail.com>
2021-01-10 17:06:33 +0200

    adding dotenv for keys

--- components/UnitPicker.js
+++ components/UnitPicker.js
@@ -1,31 +1,36 @@
 import React from 'react'
-import { View, StyleSheet, Platform} from 'react-native'
+import { View, StyleSheet, Platform } from 'react-native'
 import { Picker } from '@react-native-community/picker'
-export default function UnitPicker({ unitSystem, setUnitSystem }) {
-    return (
-        <View style={styles.unitSystem}>
-            <Picker selectedValue={unitSystem} onValueChange={(item)=> setUnitSystem(item)} 
-                    mode="dropdown" itemStyle={{fontSize: 20}}>
-                <Picker.Item label="C°" value="metric"/>
-                <Picker.Item label="F°" value="imperial"/>
 
+export default function UnitsPicker({ unitsSystem, setUnitsSystem }) {
+    return (
+        <View style={styles.unitsSystem}>
+            <Picker
+                selectedValue={unitsSystem}
+                onValueChange={(item) => setUnitsSystem(item)}
+                mode="dropdown"
+                itemStyle={{ fontSize: 12 }}
+            >
+                <Picker.Item label="C°" value="metric" />
+                <Picker.Item label="F°" value="imperial" />
             </Picker>
         </View>
     )
 }
-
-const styles = StyleSheet.create({ 
-    unitSystem: {
+const styles = StyleSheet.create({
+    unitsSystem: {
         position: 'absolute',
         ...Platform.select({
-            ios:{
-                top:-10,
+            ios: {
+                top: -30,
             },
             android: {
-                top: 20,
-            }
+                top: 30,
+            },
         }),
+
+        left: 20,
         height: 50,
         width: 100,
-    }
+    },
 })
---
+++ keys.js
@@ -0,0 +1,1 @@
+export const WEATHER_API_KEY = 'd8eee5e8e18aa41e451dd8d410253ec1';
